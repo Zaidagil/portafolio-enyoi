58432ee9dd492f65ece9cddcf6676a5aa11040c2
zaidagil <cheoacero4@gmail.com>
2024-11-14 22:32:46 -0500

    version inicial

---
+++ script.js
@@ -0,0 +1,13 @@
+const modal= document.getElementById("modal");
+
+
+function  openModal(){ 
+modal.style.display= "flex"
+    
+ //  document.getElementsByClassName("modal-content").style.display="flex";
+
+} 
+function closeModal(){
+    document.getElementById("modal").openModal.display="none";
+  
+}
